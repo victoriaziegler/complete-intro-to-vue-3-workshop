405938f760b4bb17b3fd02d69fe65fd370d6f74d
Victoria Ziegler <toriziegler@cox.net>
2023-01-11 09:37:30 -0800

    Added slots

--- EmperorsNewGrove-Forum/src/App.vue
+++ EmperorsNewGrove-Forum/src/App.vue
@@ -1,4 +1,5 @@
 <script>
+import BaseLayout from "./components/BaseLayout.vue"
 import ListCharacters from "./components/ListCharacters.vue"
 import AddCharacter from "./components/AddCharacter.vue"
 import Counter from "./components/Counter.vue"
@@ -6,7 +7,6 @@ import Statistics from "./components/CharacterStatistics.vue"
 
 export default {
   data: () => ({
-    favoritesList: [],
     listOfCharacters: [
       { name: 'Kuzco', species: ['Human', 'Llama'] },
       { name: 'Pacha', species: ['Human'] },
@@ -19,16 +19,22 @@ export default {
     Statistics,
     AddCharacter,
     Counter,
+    BaseLayout,
   }
 }
 </script>
 
 <template>
+  <BaseLayout>
+    <template v-slot:one>
+      <AddCharacter :characters="listOfCharacters" />
+    </template>
+    <template v-slot:two>
+      <Statistics :characters="listOfCharacters" />
+    </template>
+  </BaseLayout>
+  <hr>
   <ListCharacters :characters="listOfCharacters" />
-  <hr>
-  <Statistics :characters="listOfCharacters" />
-  <hr>
-  <AddCharacter :characters="listOfCharacters" />
   <hr>
   <Counter />
 </template>
