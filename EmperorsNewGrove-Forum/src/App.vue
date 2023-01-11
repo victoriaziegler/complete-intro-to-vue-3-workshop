4b2f94026cf72563bfecc911b92b18d74e5f4888
Victoria Ziegler <toriziegler@cox.net>
2023-01-11 10:05:31 -0800

    Pulling Pokemon API data from web

--- EmperorsNewGrove-Forum/src/App.vue
+++ EmperorsNewGrove-Forum/src/App.vue
@@ -1,7 +1,22 @@
-<script></script>
+<script>
+export default {
+  data: () => ({
+    pokedex: [1, 2, 3],
+  }),
+  methods: {
+    async fetchPokemon() {
+      this.pokedex = await fetch('https://pokeapi.co/api/v2/pokemon/ditto').then(response => response.json())
+    }
+  }
+}
+</script>
 
 <template>
-  <h1>New App</h1>
+  <h1>Practice Pulling from Back End</h1>
+  <pre>{{ pokedex }}</pre>
+  <button @click="fetchPokemon">Fetch Pokemon</button>
 </template>
 
-<style></style>
+<style>
+
+</style>
