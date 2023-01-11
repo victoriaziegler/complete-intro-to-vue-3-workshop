a4ec6f996d59251beea682dd276ca19d358b4aea
Victoria Ziegler <toriziegler@cox.net>
2023-01-11 15:16:27 -0800

    Created navbar and connected a JSON user API to fetch user data

--- EmperorsNewGrove-Forum/src/App.vue
+++ EmperorsNewGrove-Forum/src/App.vue
@@ -1,4 +1,7 @@
 <script>
+import HomePage from "./components/HomePage.vue"
+import LoginPage from "./components/LoginPage.vue"
+import UserPage from "./components/UserPage.vue"
 import BaseLayout from "./components/BaseLayout.vue"
 import ListCharacters from "./components/ListCharacters.vue"
 import AddCharacter from "./components/AddCharacter.vue"
@@ -7,6 +10,7 @@ import Statistics from "./components/CharacterStatistics.vue"
 
 export default {
   data: () => ({
+    currentPage: "Home",
     listOfCharacters: [
       { name: 'Kuzco', species: ['Human', 'Llama'] },
       { name: 'Pacha', species: ['Human'] },
@@ -15,16 +19,43 @@ export default {
     ]
   }),
   components: {
+    HomePage,
+    LoginPage,
+    UserPage,
     ListCharacters,
     Statistics,
     AddCharacter,
     Counter,
     BaseLayout,
-  }
+  },
+  methods: {
+    showHomePage() {
+      this.currentPage = "Home"
+    },
+    showLoginPage() {
+      this.currentPage = "Login"
+    },
+    showUserPage() {
+      this.currentPage = "User"
+    }
+  },
 }
 </script>
 
 <template>
+  <header class="header">
+    <span class="logo">
+      <img src="@/assets/vue-heart.png" width="30" />C'est La Vue
+    </span>
+    <nav class="nav">
+      <a href="#" @click.prevent="showHomePage">Home</a>
+      <a href="#" @click.prevent="showLoginPage">Login</a>
+      <a href="#" @click.prevent="showUserPage">User</a>
+    </nav>
+  </header>
+  <HomePage v-if="currentPage === 'Home'" />
+  <LoginPage v-else-if="currentPage === 'Login'" />
+  <UserPage v-else-if="currentPage === 'User'" />
   <BaseLayout>
     <template v-slot:one>
       <AddCharacter :characters="listOfCharacters" />
@@ -38,3 +69,44 @@ export default {
   <hr>
   <Counter />
 </template>
+
+<style>
+* {
+  box-sizing: border-box;
+  font-family: "Inter", sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 1rem;
+  border-bottom: 1px solid #ccc;
+}
+
+span.logo {
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+
+span.logo img {
+  margin-right: 8px;
+}
+
+.nav {
+  display: flex;
+  align-items: center;
+}
+
+.nav a {
+  padding: 0.5rem;
+  font-size: 0.9rem;
+}
+
+.nav a:last-child {
+  padding-right: 0;
+}
+</style>
---
+++ EmperorsNewGrove-Forum/src/components/UserPage.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    data: () => ({
+        userList: []
+    }),
+    methods: {
+        async fetchUsers() {
+            this.userList = await fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Users</h1>
+    <button @click="fetchUsers">Fetch Users</button>
+    <div class="user-grid">
+        <ul v-for="user in userList" :key="`user-${user.id}`">
+            <li class="user-item">{{ user.name }}: {{ user.company.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<style>
+.user-grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    border: 5px dotted palevioletred
+}
+
+.user-item {
+    padding: 10px;
+    border: 1px solid black;
+}
+</style>
