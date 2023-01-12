aa95ab38692e4dbc17f423d14d59e7534a191cef
Victoria Ziegler <toriziegler@cox.net>
2023-01-12 11:40:02 -0800

    script setup

--- EmperorsNewGrove-Forum/src/components/UserPage.vue
+++ EmperorsNewGrove-Forum/src/components/UserPage.vue
@@ -1,28 +1,18 @@
-<script>
+<script setup>
 import UserCard from "./UserCard.vue";
 import { reactive } from "vue";
-export default {
-    components: {
-        UserCard,
-    },
-    async setup() {
-        const state = reactive({
-            userList: [],
-        })
 
-        async function fetchUsers() {
-            const response = await fetch(
-                "https://jsonplaceholder.typicode.com/users"
-            ).then((response) => response.json())
-            return response;
-        }
-        state.userList = await fetchUsers()
-        return {
-            state,
-            fetchUsers,
-        }
-    },
+const state = reactive({
+    userList: [],
+})
+
+async function fetchUsers() {
+    const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+    ).then((response) => response.json())
+    return response;
 }
+state.userList = await fetchUsers()
 </script>
 
 <template>
